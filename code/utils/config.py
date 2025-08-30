3e460e43c0f9ff50c6eb8df8b10505208d558c92
CZH <81380774+czhpage@users.noreply.github.com>
2025-08-30 22:57:47 +0300

    Update config.py

--- code/utils/config.py
+++ code/utils/config.py
@@ -3,7 +3,7 @@ class Config:
     def __init__(self,task):
         if task == "synapse":
             self.base_dir = '/home/chenzh/FILE1/Datasets/Synapse'
-            self.save_dir = '/home/chenzh/FILE1/DHC/DBANet/synapse_data'
+            self.save_dir = '/home/chenzh/FILE1/DBANet/synapse_data'
             self.patch_size = (64, 128, 128)
             self.num_cls = 14
             self.num_channels = 1
@@ -11,7 +11,7 @@ class Config:
             self.early_stop_patience = 100
         elif task == "amos":
             self.base_dir = '/home/chenzh/FILE1/Datasets/amos22'
-            self.save_dir = '/home/chenzh/FILE1/DHC/DBANet/amos_data'
+            self.save_dir = '/home/chenzh/FILE1/DBANet/amos_data'
             self.patch_size = (64, 128, 128)
             self.num_cls = 16
             self.num_channels = 1
@@ -19,7 +19,7 @@ class Config:
             self.early_stop_patience = 50
         else: # word
             self.base_dir = '/home/chenzh/FILE1/Datasets/wordv01'
-            self.save_dir = '/home/chenzh/FILE1/DHC/DBANet/word_data'
+            self.save_dir = '/home/chenzh/FILE1/DBANet/word_data'
             self.patch_size = (64, 128, 128)
             self.num_cls = 17
             self.num_channels = 1
